71eb65d97243414bd2caa7fae77a37a4abf2adf6
김나경 <1030n@naver.com>
2026-02-20 18:48:00 +0900

    :recycle: Refactor: Migrate CRUD and session layer to AsyncSession

--- api/routers/done.py
+++ api/routers/done.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.crud.done as done_crud
 import api.schemas.done as done_schema
@@ -8,24 +8,24 @@ from api.db import get_db
 router = APIRouter()
 
 @router.put("/tasks/{task_id}/done", response_model=done_schema.DoneResponse)
-async def mark_task_as_done(task_id: int, db: Session = Depends(get_db)):
+async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
 
     # task_id를 통해 조회
-    original = done_crud.get_done(db, task_id=task_id)
+    original = await done_crud.get_done(db, task_id=task_id)
 
     # 있는 경우 예외 처리
     if original is not None:
         raise HTTPException(status_code=400, detail="Done already exists")
 
     # done 생성 후 반환
-    return done_crud.create_done(db, task_id)
+    return await done_crud.create_done(db, task_id)
 
 @router.delete("/tasks/{task_id}/done", response_model=None)
-async def unmark_task_as_done(task_id: int, db: Session = Depends(get_db)):
+async def unmark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
 
-    original = done_crud.get_done(db, task_id=task_id)
+    original = await done_crud.get_done(db, task_id=task_id)
 
     if original is None:
         raise HTTPException(status_code=404, detail="Done task not found")
     
-    return done_crud.delete_done(db, original)
+    return await done_crud.delete_done(db, original)
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.schemas.task as task_schema
 import api.crud.task as task_crud
@@ -8,33 +8,37 @@ from api.db import get_db
 router = APIRouter()
 
 @router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
-    return task_crud.create_task(db, task_body)
+async def list_tasks(
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+    ):
+    return await task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
+async def update_task(
+    task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
+    ):
 
     # task_id를 통해 조회
-    original = task_crud.get_task(db, task_id=task_id)
+    original = await task_crud.get_task(db, task_id=task_id)
 
     # 없는 경우 예외처리
     if original is None:
         raise HTTPException(status_code=404, detail="Task not found")
     
     # update 로직 실행 후 반환
-    return task_crud.update_task(db, task_body, original=original)
+    return await task_crud.update_task(db, task_body, original=original)
     
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int, db: Session = Depends(get_db)):
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
 
-    original = task_crud.get_task(db, task_id=task_id)
+    original = await task_crud.get_task(db, task_id=task_id)
 
     if original is None:
         raise HTTPException(status_code=404, detail="Task not found")
     
-    return task_crud.delete_task(db, original)
+    return await task_crud.delete_task(db, original)
