297e470dbd061257770fde1d0ed6ff2a844f380a
김나경 <1030n@naver.com>
2026-02-20 13:15:00 +0900

    :sparkles: Feat: Add Schema - Request

--- api/routers/task.py
+++ api/routers/task.py
@@ -8,14 +8,14 @@ router = APIRouter()
 async def list_tasks():
     return [task_schema.Task(id=1, title="첫 번째 Todo 작업")]
 
-@router.post("/tasks")
-async def create_task():
-    pass
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_task(task: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task.model_dump())
 
-@router.put("/tasks/{task_id}")
-async def update_task():
-    pass
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.model_dump())
 
 @router.delete("/tasks/{task_id}")
-async def delete_task():
-    pass
+async def delete_task(task_id: int):
+    return
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,6 +1,20 @@
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
-    id: int
+class TaskBase(BaseModel):
     title: str | None = Field(None, exmaple="코딩 공부하기")
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="완료 플래그")
+
+    class Config:
+        orm_mode = True
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
